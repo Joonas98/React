34c345f9c962376012341b29a982ba4f11439d9a
Joonas <jsuuronen9@gmail.com>
2023-06-13 20:00:00 +0300

    fixing

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/React/",
   plugins: [react()],
-  // build: {
-  //   outDir: "C:/Users/jsuur/Desktop/React/build",
-  // },
 });
